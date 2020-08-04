d594031cefcbb7b2aaafe0d3929de6b26ec1243d
Auxione <cem0509@hotmail.com>
2020-08-04 17:57:31 +0300

    Major overhaul.

--- src/curio/graphics/Mesh.java
+++ src/curio/graphics/Mesh.java
@@ -1,14 +1,50 @@
-package graphics.renderer3d;
+package graphics;
 
-public interface Mesh {
+import common.buffers.IndexBuffer;
+import common.buffers.VertexBuffer;
+import core.EngineSettings;
+import platform.opengl.OGL_VertexArray;
 
-	public void uploadData();
+public abstract class Mesh {
+	public VertexBuffer vertexBuffer;
+	public IndexBuffer indexBuffer;
 
-	public void uploadSubData(int VBindex, int IBindex);
+	public enum DrawType {
+		STATIC_DRAW, DYNAMIC_DRAW, STREAM_DRAW
+	}
 
-	public void bind();
+	public enum DrawMode {
+		TRIANGLES, LINES, LINE_STRIP, LINE_LOOP, TRIANGLE_STRIP, TRIANGLE_FAN, POINTS
+	}
 
-	public int getID();
+	public static Mesh createInstance(DrawMode mode, DrawType bufferType, VertexBuffer vertexBuffer,
+			IndexBuffer indexBuffer) {
+		switch (EngineSettings.renderer) {
 
-	public void drawIndexed(int count);
+		default:
+			return new OGL_VertexArray(mode, bufferType, vertexBuffer, indexBuffer);
+		}
+	}
+
+	protected Mesh(int maxTriangles, int bytesPerData, int[] dataSizes) {
+		this.vertexBuffer = new VertexBuffer(maxTriangles * 8, bytesPerData, dataSizes);
+		this.indexBuffer = new IndexBuffer(maxTriangles * 12);
+	}
+
+	protected Mesh(VertexBuffer vertexBuffer, IndexBuffer indexBuffer) {
+		this.vertexBuffer = vertexBuffer;
+		this.indexBuffer = indexBuffer;
+	}
+
+	public abstract void uploadData();
+
+	public abstract void uploadSubData(int VBindex, int IBindex);
+
+	public abstract void bind();
+
+	public abstract int getID();
+
+	public abstract void renderIndexed(int count);
+
+	public abstract void renderArrays(int first, int count);
 }
