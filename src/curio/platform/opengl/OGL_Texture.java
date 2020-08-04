1580f51d2828d33ae130c9b8f71eb4a9589ca41b
Auxione <cem0509@hotmail.com>
2020-08-04 18:13:18 +0300

    Reworked for FrameBuffer.

--- src/curio/platform/opengl/OGL_Texture.java
+++ src/curio/platform/opengl/OGL_Texture.java
@@ -1,13 +1,18 @@
 package platform.opengl;
 
+import static org.lwjgl.opengl.GL11.GL_RGB;
+import static org.lwjgl.opengl.GL11.GL_TEXTURE_2D;
+import static org.lwjgl.opengl.GL11.GL_UNSIGNED_BYTE;
+import static org.lwjgl.opengl.GL11.glTexImage2D;
 import static org.lwjgl.opengl.GL45.*;
 
+import org.lwjgl.system.MemoryUtil;
+
 import common.Console;
-import common.utilities.ImageBuffer;
+import common.buffers.ImageBuffer;
 import common.utilities.NativeObject;
 import common.utilities.NativeObjectManager;
 import graphics.Texture;
-import graphics.renderer2d.Renderer2D;
 
 public class OGL_Texture implements Texture, NativeObject, OGL_Object {
 	private static int nextTextureSlot = 0;
@@ -16,37 +21,59 @@ public class OGL_Texture implements Texture, NativeObject, OGL_Object {
 	private ImageBuffer imageBuffer;
 	private int textureSlot = nextTextureSlot;
 
-	public OGL_Texture() {
+	private int width, height;
+
+	public OGL_Texture(int width, int height, boolean allocate) {
 		this.ID = glGenTextures();
+		this.width = width;
+		this.height = height;
+		if (allocate) {
+			GPUMemAlloc();
+		}
 		NativeObjectManager.register(this);
 	}
 
 	public OGL_Texture(ImageBuffer imageBuffer) {
-		this.ID = glGenTextures();
+		this(imageBuffer.getWidth(), imageBuffer.getHeight(), false);
 		this.imageBuffer = imageBuffer;
-		GPULoadMethod(0);
+		GPUMemLoad(imageBuffer);
 		NativeObjectManager.register(this);
 	}
 
-	@Override
-	public void GPULoadMethod(int parameter) {
-		if (OGL_Texture.nextTextureSlot > Renderer2D.TEXTURESAMPLERSIZE) {
+	public void GPUMemLoad(ImageBuffer imageBuffer) {
+		if (OGL_Texture.nextTextureSlot > OGL_Renderer.TEXTURESAMPLERSIZE) {
 			Console.severe(this, "Failed to create textures.");
 			return;
 		}
-
 		bind();
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-
+		param();
 		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
 		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, imageBuffer.getWidth(), imageBuffer.getHeight(), 0, GL_RGBA,
 				GL_UNSIGNED_BYTE, imageBuffer.getData());
 
-		OGL_Texture.nextTextureSlot += 1;// TODO: testing
+		OGL_Texture.nextTextureSlot += 1;
 		glBindTextureUnit(this.textureSlot, this.getID());
+	}
+
+	public void GPUMemAlloc() {
+		if (OGL_Texture.nextTextureSlot > OGL_Renderer.TEXTURESAMPLERSIZE) {
+			Console.severe(this, "Failed to create textures.");
+			return;
+		}
+		bind();
+		param();
+		glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, this.width, this.height, 0, GL_RGB, GL_UNSIGNED_BYTE, MemoryUtil.NULL);
+
+		OGL_Texture.nextTextureSlot += 1;
+		glBindTextureUnit(this.textureSlot, this.getID());
+	}
+
+	private void param() {
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+		glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
 	}
 
 	@Override
@@ -67,12 +94,12 @@ public class OGL_Texture implements Texture, NativeObject, OGL_Object {
 
 	@Override
 	public final int getWidth() {
-		return this.imageBuffer.getWidth();
+		return this.width;
 	}
 
 	@Override
 	public final int getHeight() {
-		return this.imageBuffer.getHeight();
+		return this.height;
 	}
 
 	@Override
