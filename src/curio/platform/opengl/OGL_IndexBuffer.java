87e46490fb235720c35321c968f5d5cb0f734192
Auxione <cem0509@hotmail.com>
2020-08-04 18:07:42 +0300

    Major changes.

--- src/curio/platform/opengl/OGL_IndexBuffer.java
+++ src/curio/platform/opengl/OGL_IndexBuffer.java
@@ -1,76 +1,42 @@
 package platform.opengl;
 
-import java.nio.IntBuffer;
-
 import static org.lwjgl.opengl.GL45.*;
-import org.lwjgl.system.MemoryUtil;
 
-import graphics.renderer3d.Indexable;
+import common.buffers.IndexBuffer;
+import graphics.Mesh.DrawType;
 
 public class OGL_IndexBuffer extends OGL_Buffer {
-	private IntBuffer intBuffer;
-	private int maxIndexNumber = 0;
-	private int currentIndexOffset = 0;
-
-	public OGL_IndexBuffer(Method method, int capacity) {
+	public OGL_IndexBuffer(DrawType method) {
 		super(method);
-		this.intBuffer = MemoryUtil.memAllocInt(capacity);
 	}
 
-	public void put(int[] indices) {
-		this.maxIndexNumber = 0;
-		for (int i = 0; i < indices.length; i++) {
-			if (this.maxIndexNumber < indices[i]) {
-				this.maxIndexNumber = indices[i];
-			}
-
-			this.intBuffer.put(indices[i] + this.currentIndexOffset);
-		}
-		this.currentIndexOffset += maxIndexNumber + 1;
+	public void uploadSubData(IndexBuffer indexBuffer, int index) {
+		indexBuffer.flip();
+		indexBuffer.resetIndex();
+		bind(indexBuffer);
+		glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, index * Integer.BYTES, indexBuffer.getData());
+		indexBuffer.clear();
 	}
 
-	public void put(int index) {
-		index += this.currentIndexOffset;
-		this.intBuffer.put(index);
-		this.currentIndexOffset += 1;
+	public void uploadData(IndexBuffer indexBuffer) {
+		indexBuffer.flip();
+		indexBuffer.resetIndex();
+		bind(indexBuffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexBuffer.getData(), super.getDrawMethod());
+		indexBuffer.clear();
 	}
 
-	public void put(Indexable indexable) {
-		put(indexable.getIndexArray());
+	public void GPUMemAlloc(IndexBuffer indexBuffer) {
+		bind(indexBuffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexBuffer.capacity(), super.getDrawMethod());
 	}
 
-	public void uploadSubData(int index) {
-		this.intBuffer.flip();
-		this.currentIndexOffset = 0;
-		bind();
-		glBufferSubData(GL_ELEMENT_ARRAY_BUFFER, index * Integer.BYTES, this.intBuffer);
-		this.intBuffer.clear();
+	public void GPUMemLoad(IndexBuffer indexBuffer) {
+		bind(indexBuffer);
+		glBufferData(GL_ELEMENT_ARRAY_BUFFER, indexBuffer.getData(), super.getDrawMethod());
 	}
 
-	public void uploadData() {
-		this.intBuffer.flip();
-		this.currentIndexOffset = 0;
-		bind();
-		glBufferData(GL_ELEMENT_ARRAY_BUFFER, this.intBuffer, super.getDrawMethod());
-		this.intBuffer.clear();
-	}
-
-	@Override
-	public void GPULoadMethod(int parameter) {
-		bind();
-		if (parameter == 1) {
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, this.intBuffer.capacity(), super.getDrawMethod());
-		} else {
-			glBufferData(GL_ELEMENT_ARRAY_BUFFER, this.intBuffer, super.getDrawMethod());
-		}
-	}
-
-	@Override
-	public void bind() {
+	public void bind(IndexBuffer indexBuffer) {
 		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, super.getID());
-	}
-
-	public int getLimit() {
-		return this.intBuffer.limit();
 	}
 }
