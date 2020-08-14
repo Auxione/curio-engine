eefbc091437ace869eb187702029a37b906d2b76
Auxione <cem0509@hotmail.com>
2020-08-15 00:24:05 +0300

    Added get method.

--- src/curio/common/buffers/VertexBuffer.java
+++ src/curio/common/buffers/VertexBuffer.java
@@ -9,18 +9,18 @@ import graphics.renderer2d.Vertex2;
 
 public class VertexBuffer implements NativeObject {
 	private FloatBuffer floatBuffer;
-	private int bytesPerData;
+	private int bytesPerVertex;
 	private int[] vertexDataSizes;
 
-	public VertexBuffer(int capacity, int bytesPerData, int[] dataSizes) {
+	public VertexBuffer(int capacity, int bytesPerVertex, int[] dataformat) {
 		this.floatBuffer = MemoryUtil.memAllocFloat(capacity);
-		this.bytesPerData = bytesPerData;
-		this.vertexDataSizes = dataSizes;
+		this.bytesPerVertex = bytesPerVertex;
+		this.vertexDataSizes = dataformat;
 		NativeObjectManager.register(this);
 	}
 
 	public VertexBuffer(Vertex2[] vertices) {
-		this(vertices.length * Vertex2.SIZE, Vertex2.BYTES, Vertex2.DATASIZES);
+		this(vertices.length * Vertex2.SIZE, Vertex2.BYTES, Vertex2.DATAFORMAT);
 		put(vertices);
 	}
 
@@ -28,10 +28,18 @@ public class VertexBuffer implements NativeObject {
 		this.floatBuffer.put(Vertex2.toArray(vertex));
 	}
 
+	public void put(int index, Vertex2 vertex) {
+		this.floatBuffer.put(index * bytesPerVertex, Vertex2.toArray(vertex));
+	}
+
 	public void put(Vertex2[] vertices) {
 		for (int i = 0; i < vertices.length; i++) {
 			put(vertices[i]);
 		}
+	}
+
+	public float get(int index) {
+		return floatBuffer.get(index);
 	}
 
 	@Override
@@ -59,8 +67,8 @@ public class VertexBuffer implements NativeObject {
 		return this.floatBuffer.limit();
 	}
 
-	public int getBytesPerData() {
-		return this.bytesPerData;
+	public int getBytesPerVertex() {
+		return this.bytesPerVertex;
 	}
 
 	public int capacity() {
