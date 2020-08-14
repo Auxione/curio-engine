6e8bf1f98cbc40813845b3a7d7057196949a3a7d
Auxione <cem0509@hotmail.com>
2020-08-14 23:43:42 +0300

    Added methods to create Vertices from shape and images.

--- src/curio/graphics/VertexFactory.java
+++ src/curio/graphics/VertexFactory.java
@@ -1,5 +1,7 @@
 package graphics;
 
+import common.geom.Shape2;
+import graphics.renderer2d.Image;
 import graphics.renderer2d.Vertex2;
 
 public class VertexFactory {
@@ -15,6 +17,26 @@ public class VertexFactory {
 			}
 		}
 
+		return quadVertices;
+	}
+
+	public static Vertex2[] fromShape(Shape2 shape) {
+		Vertex2[] quadVertices = new Vertex2[shape.getDefaultPoints().length];
+		for (int i = 0; i < quadVertices.length; i++) {
+			quadVertices[i] = new Vertex2();
+			quadVertices[i].position.set(shape.getDefaultPoints()[i]);
+		}
+		return quadVertices;
+	}
+
+	public static Vertex2[] fromImage(Image image) {
+		Vertex2[] quadVertices = new Vertex2[image.getDefaultPoints().length];
+		for (int i = 0; i < quadVertices.length; i++) {
+			quadVertices[i] = new Vertex2();
+			quadVertices[i].position.set(image.getDefaultPoints()[i]);
+			quadVertices[i].textureSlot = image.texture.getSlot();
+		}
+		Vertex2.setTexturePositionRect(quadVertices[0], quadVertices[1], quadVertices[2], quadVertices[3], 0, 0, 1, 1);
 		return quadVertices;
 	}
 }
