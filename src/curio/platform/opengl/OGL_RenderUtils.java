4a7a40530650d45ce090fa3694c2a529ed10fbd1
Auxione <cem0509@hotmail.com>
2020-08-15 00:17:30 +0300

    Import cleanup, implemented abstract methods.

--- src/curio/platform/opengl/OGL_RenderUtils.java
+++ src/curio/platform/opengl/OGL_RenderUtils.java
@@ -1,12 +1,6 @@
 package platform.opengl;
 
 import static org.lwjgl.system.MemoryUtil.memByteBuffer;
-import static org.lwjgl.opengl.GL11.GL_MAX_TEXTURE_SIZE;
-import static org.lwjgl.opengl.GL11.GL_RENDERER;
-import static org.lwjgl.opengl.GL11.GL_VENDOR;
-import static org.lwjgl.opengl.GL11.glGetInteger;
-import static org.lwjgl.opengl.GL11.glGetString;
-import static org.lwjgl.opengl.GL20.GL_MAX_TEXTURE_IMAGE_UNITS;
 import static org.lwjgl.opengl.GL45.*;
 import org.lwjgl.opengl.GLDebugMessageCallbackI;
 import org.lwjgl.system.MemoryUtil;
@@ -16,6 +10,7 @@ import graphics.RenderUtilities;
 import graphics.renderer2d.Renderer2D;
 
 public class OGL_RenderUtils extends RenderUtilities {
+
 	@Override
 	public void wireFrameMode(int mode) {
 		if (mode == 1) {
@@ -35,6 +30,27 @@ public class OGL_RenderUtils extends RenderUtilities {
 				Console.severe(renderer2d, MemoryUtil.memUTF8(memByteBuffer(message, length)));
 			}
 		}, 0);
+	}
+
+	@Override
+	public void cullFaces(boolean value, CullingFace face, boolean ccw) {
+		if (value) {
+			glEnable(GL_CULL_FACE);
+		} else {
+			glDisable(GL_CULL_FACE);
+		}
+		if (face == CullingFace.Front) {
+			glCullFace(GL_FRONT);
+		} else if (face == CullingFace.Back) {
+			glCullFace(GL_BACK);
+		} else {
+			glCullFace(GL_FRONT_AND_BACK);
+		}
+		if (ccw) {
+			glFrontFace(GL_CCW);
+		} else {
+			glFrontFace(GL_CW);
+		}
 	}
 
 	@Override
