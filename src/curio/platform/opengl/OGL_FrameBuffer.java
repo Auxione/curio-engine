d25e3d3cf32e501e110b9296adf57b35f08d1751
Auxione <cem0509@hotmail.com>
2020-08-14 23:48:22 +0300

    Modified to work with OGL_Texture

--- src/curio/platform/opengl/OGL_FrameBuffer.java
+++ src/curio/platform/opengl/OGL_FrameBuffer.java
@@ -1,21 +1,39 @@
 package platform.opengl;
 
+import common.Console;
 import common.utilities.NativeObject;
+import graphics.Color;
+import graphics.FrameBuffer;
+import graphics.Texture;
+
 import static org.lwjgl.opengl.GL45.*;
 
-public class OGL_FrameBuffer implements OGL_Object, NativeObject {
+public class OGL_FrameBuffer extends FrameBuffer implements OGL_Object, NativeObject {
 	private int id;
 	private OGL_Texture oglTexture;
 
 	public OGL_FrameBuffer(int width, int height) {
 		this.id = glGenFramebuffers();
-		bind();
 		this.oglTexture = new OGL_Texture(width, height, true);
+
+		bind();
 		glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, this.oglTexture.getID(), 0);
+
+		if (glCheckFramebufferStatus(GL_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+			Console.severe(this, "Framebuffer is not completed.");
+		}
+	}
+
+	public void clear(Color color) {
+		if (color != null) {
+			glClearColor(color.x, color.y, color.z, color.w);
+			glClear(GL_COLOR_BUFFER_BIT);
+		}
 	}
 
 	public void bind() {
-		glBindFramebuffer(GL_FRAMEBUFFER, id);
+		glBindFramebuffer(GL_FRAMEBUFFER, getID());
+		glViewport(0, 0, getWidth(), getHeight());
 		this.oglTexture.bind();
 	}
 
@@ -25,16 +43,26 @@ public class OGL_FrameBuffer implements OGL_Object, NativeObject {
 
 	@Override
 	public void terminate() {
-		glDeleteFramebuffers(id);
+		glDeleteFramebuffers(getID());
 	}
 
-	public OGL_Texture getTexture() {
+	public Texture getTexture() {
 		return this.oglTexture;
 	}
 
 	@Override
 	public int getID() {
 		return id;
+	}
+
+	@Override
+	public int getWidth() {
+		return this.oglTexture.getWidth();
+	}
+
+	@Override
+	public int getHeight() {
+		return this.oglTexture.getHeight();
 	}
 
 }
