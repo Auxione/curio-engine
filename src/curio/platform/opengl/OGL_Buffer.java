908e2726a9ef7e10715f93a2fb5e91031c221c7f
Auxione <cem0509@hotmail.com>
2020-08-19 06:34:29 +0300

    Updated to new implementation.

--- src/curio/platform/opengl/OGL_Buffer.java
+++ src/curio/platform/opengl/OGL_Buffer.java
@@ -8,31 +8,29 @@ import graphics.Mesh.DrawType;
 
 public abstract class OGL_Buffer implements OGL_Object, NativeObject {
 	private int id;
-	private int DRAW_METHOD;
+	
+	protected OGL_Buffer() {
+		this.id = glGenBuffers();
+		NativeObjectManager.register(this);
+	}
 
-	protected OGL_Buffer(DrawType drawType) {
+	public static int getOGLType(DrawType drawType) {
 		if (drawType.equals(DrawType.DYNAMIC_DRAW)) {
-			this.DRAW_METHOD = GL_DYNAMIC_DRAW;
+			return GL_DYNAMIC_DRAW;
 		}
 
 		else if (drawType.equals(DrawType.STREAM_DRAW)) {
-			this.DRAW_METHOD = GL_STREAM_DRAW;
+			return GL_STREAM_DRAW;
 		}
 
 		else {
-			this.DRAW_METHOD = GL_STATIC_DRAW;
+			return GL_STATIC_DRAW;
 		}
-		this.id = glGenBuffers();
-		NativeObjectManager.register(this);
 	}
 
 	@Override
 	public void terminate() {
 		glDeleteBuffers(getID());
-	}
-
-	public int getDrawMethod() {
-		return this.DRAW_METHOD;
 	}
 
 	@Override
